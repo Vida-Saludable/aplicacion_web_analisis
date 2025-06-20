575ee405178b6cef39a98628b82c45f2c7565230
Nestor Villca <nestorvillca147@gmail.com>
2025-06-20 00:30:46 -0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // const  baseURL='http://127.0.0.1:8000'
-const  baseURL='http://127.0.0.1:8000'
+// const  baseURL='http://127.0.0.1:8000'
 // const  baseURL='http://127.0.0.1:8001'
-// const  baseURL='http://145.223.27.25:8081'
+const  baseURL='http://145.223.27.25:8081'
 export const environment = {
   production: true,
   baseLogin: baseURL+ '/login/',
