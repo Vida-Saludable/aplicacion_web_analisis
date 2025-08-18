2f57baf7a6cf8c86ed2a5e4d54757b895ca1dc7f
Nestor Villca  Mollisaca <105319566+Nestor147@users.noreply.github.com>
2025-08-18 11:42:30 -0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 // const  baseURL='http://127.0.0.1:8000'
 // const  baseURL='http://127.0.0.1:8000'
-// const  baseURL='http://127.0.0.1:8000'
-const  baseURL='http://145.223.27.25:8081'
+const  baseURL='http://127.0.0.1:8000'
+//https://api.vidasaludable.cloud/ apunta
+// const  baseURL = "https://api.vidasaludable.cloud"; // producion servidor
+// const  baseURL='http://145.223.27.25:8081'
 export const environment = {
   production: true,
   baseLogin: baseURL+ '/login/',
